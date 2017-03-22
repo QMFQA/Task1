315d6576c82556e8a34469f3fc6c8da03dd4778e
amorozova <amorozova@CHE-D-AM03.rocketsoftware.com>
2017-03-22 09:51:39 +0500

    Fixed

--- src/task/Circle.java
+++ src/task/Circle.java
@@ -1,9 +1,9 @@
 package task;
 
 public class Circle extends Figure{
-	String figure = new String("Circle");
-	final double pi = Math.PI;
+	public final double pi = Math.PI;
 	private double radius;
+	
 	public Circle (double radius){
 		this.radius = radius;
 	}
