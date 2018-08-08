1a67bce5365cb0162e926c7b93fb152015df1f44
Artem <iskhakovartem@gmail.com>
2018-08-08 11:21:10 +0500

    Update Main.java

--- src/task/Main.java
+++ src/task/Main.java
@@ -4,13 +4,7 @@ package task;
 public class Main {
 	
 	public static void main(String[] args)
-	{
-		Figure[] figures = new Figure[3];
-		figures[0] = new Circle(10);
-		figures[1] = new Rectangle(12.5, 10);
-		figures[2] = new Square(15.75);
-		for (Figure figure : figures)
-			System.out.println(figure + " perimeter is " + figure.perimeter());
-	}
+	
+			System.out.println("Test");
 	
 }
